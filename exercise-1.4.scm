e39d07b69a5e0b5e1501245b599cc71dd4040f62
edjohn <edwardjustinjohn@gmail.com>
2020-04-02 21:57:01 -0700

    Add exercise 1.4, exercise 1.5, and sqrt-iter program

---
+++ exercise-1.4.scm
@@ -0,0 +1,4 @@
+;Exercise 1.4
+;Perform a conditional check to determine whether parameter 'b' is greater than 0.
+;If this evaluates to true, add parameters a and b.
+;If this evaluates to false, subtract parameter b from a.
---
+++ exercise-1.5.scm
@@ -0,0 +1,3 @@
+;Exercise 1.5
+;With applicative-order evaluation, (p) would be evaluated and the program would loop indefinitely. 
+;With normal-order evaluation, (p) would never be evaluated because the program ends after the first conditional check.
