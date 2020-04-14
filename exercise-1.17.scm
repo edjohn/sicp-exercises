feeb288a8be4407d542015b6f7092754de5dba80
edjohn <edwardjustinjohn@gmail.com>
2020-04-13 19:47:55 -0700

    Add exercise 1.17

---
+++ exercise-1.17.scm
@@ -0,0 +1,16 @@
+;Exercise 1.17
+(define (fast-mult a n)
+  (fast-mult-iter 0 a n))
+
+(define (fast-mult-iter a b n)
+  (if (= n 0)
+    a
+    (if (even? n)
+      (fast-mult-iter a (double b) (halve n))
+      (fast-mult-iter (+ a b) b (- n 1)))))
+
+(define (double x)
+  (+ x x))
+
+(define (halve x)
+  (/ x 2))
