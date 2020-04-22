d26cc12f70958025e377cef97602a4889d4b7306
edjohn <edwardjustinjohn@gmail.com>
2020-04-21 20:37:24 -0700

    Add exercise 1.23

---
+++ exercise-1.23.scm
@@ -0,0 +1,34 @@
+;Exercise 1.23
+;The modified algorithm runs approximately twice as fast when tested with the 12 prime values from exercise 1.22
+
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+  (if (prime? n)
+    (report-prime (- (runtime) start-time))))
+
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+	((divides? test-divisor n) test-divisor)
+	(else (find-divisor n (next test-divisor)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (next n)
+  (if (= n 2)
+    3
+    (+ n 2)))
