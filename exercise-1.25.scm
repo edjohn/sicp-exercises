d925f7a5378ff3e5769e0fb9d70e63b0bd125ee4
edjohn <edwardjustinjohn@gmail.com>
2020-04-22 19:12:09 -0700

    Add exercise 1.25

---
+++ exercise-1.25.scm
@@ -0,0 +1,38 @@
+;Exercise 1.25
+;Using fast-expt, fast-prime takes much more time to compute when using larger numbers compared to the original version
+;This is due to the fact that the original algorithm computes successive remainders,  and thus only has to square small numbers
+;In contrast, the fast-expt algorithm does not compute any remainders until the very end, and so ends up squaring increasingly large numbers
+
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+  (if (fast-prime? n 1)
+    (report-prime (- (runtime) start-time))))
+
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+	((fermat-test n) (fast-prime? n (- times 1)))
+	(else false)))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (expmod base exponent m)
+  (remainder (fast-expt base exponent) m))
+
+(define (fast-expt b n)
+  (cond ((= n 0) 1)
+	((even? n) (square (fast-expt b (/ n 2))))
+	(else (* b (fast-expt b (- n 1))))))
+
+(define (even? n)
+  (= (remainder n 2) 0))
